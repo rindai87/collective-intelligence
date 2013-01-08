5d366278dbf07f078368d830a7887978668fc50a
norihiro.shimoda <norihiro.shimoda@gmail.com>
2013-01-08 19:11:52 +0900

    ジニ係数とエントロピーの実装

--- Python/decision_tree/treepredict.py
+++ Python/decision_tree/treepredict.py
@@ -33,3 +33,26 @@ def uniquecounts(rows):
         if r not in results: results[r] = 0
         results[r] += 1
     return results
+
+def giniimpurity(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts:
+        p1 = float(counts[k1])/total
+        for k2 in counts:
+            if k1 == k2: continue
+            p2 = float(counts[k2])/total
+            imp += p1 * p2
+    return imp
+
+def entropy(rows):
+    from math import log
+    log2 = lambda x:log(x)/log(2)
+    results = uniquecounts(rows)
+
+    ent = 0.0
+    for r in results.keys():
+        p = float(results[r])/len(rows)
+        ent = ent-p*log2(p)
+    return ent
