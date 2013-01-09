f12cc90abf1f5c39dfe658c52a1baf07f80f4436
Norihiro Shimoda <norihiro.shimdoa@gmail.com>
2013-01-10 01:32:44 +0900

    ID3アルゴリズム自体の実装と、結果を表示するprinttreeの実装

--- Python/decision_tree/treepredict.py
+++ Python/decision_tree/treepredict.py
@@ -6,7 +6,7 @@ class decisionnode:
     def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
         self.col=col
         self.value=value
-        self.results=result
+        self.results=results
         self.tb=tb
         self.fb=fb
         
@@ -54,5 +54,46 @@ def entropy(rows):
     ent = 0.0
     for r in results.keys():
         p = float(results[r])/len(rows)
-        ent = ent-p*log2(p)
+	ent = ent-p*log2(p)
     return ent
+
+def buildtree(rows, scoref=entropy):
+    if len(rows) == 0: return None
+
+    current_score = scoref(rows)
+
+    best_gain = 0.0
+    best_criteria = None
+    best_sets = None
+
+    column_count = len(rows[0]) - 1
+    for col in range(0, column_count):
+        column_values = {}
+        for row in rows:
+            column_values[row[col]] = 1
+        for value in column_values.keys():
+            (set1, set2) = divideset(rows, col, value)
+
+            p = float(len(set1))/len(rows)
+            gain = current_score - p*scoref(set1) - (1-p)*scoref(set2)
+            if gain > best_gain and len(set1) > 0 and len(set2) > 0:
+                best_gain = gain
+                best_criteria = (col, value)
+                best_sets = (set1, set2)
+    if best_gain > 0:
+        trueBranch = buildtree(best_sets[0], scoref)
+        falseBranch = buildtree(best_sets[1], scoref)
+        return decisionnode(col = best_criteria[0], value = best_criteria[1], tb = trueBranch, fb = falseBranch)
+    else:
+        return decisionnode(results = uniquecounts(rows))
+
+def printtree(tree, indent = ''):
+    if tree.results != None:
+        print str(tree.results)
+    else:
+        print str(tree.col) + ':' + str(tree.value) + '? '
+
+        print indent + 'T-> ',
+        printtree(tree.tb, indent + '  ')
+        print indent + 'F-> ',
+        printtree(tree.fb, indent + '  ')
